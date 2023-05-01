06d77d9810a9ebb2a9446cef54816c40abdad365
John Lundy <79663204+johnlundy94@users.noreply.github.com>
2023-05-01 09:02:49 -0600

    completed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img height={200} src={require("./assets/logo.png")}/>
+    </div>
+  );
+}
+
+export default App;
